d7d1e4e59ec284e75b1d62d98efa27259423bf8e
Dhanush <dhanushchmca@gmail.com>
2026-03-06 21:47:17 +0530

    Create Missingnumber.java

---
+++ Missingnumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    int missingNumber(int arr[]) {
+        int n = arr.length + 1;
+        
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        
+        for(int num : arr) {
+            actualSum += num;
+        }
+        
+        return expectedSum - actualSum;
+    }
+}
