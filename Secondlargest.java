bceb8adc38ac494c253412d49d9c9853d9525599
Dhanush <dhanushchmca@gmail.com>
2026-03-06 21:52:49 +0530

    Create Secondlargest.java

---
+++ Secondlargest.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+      if(arr.length <2) return -1;
+      int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+      for(int num : arr){
+          if(num > first){
+              second = first;
+              first = num;
+          }
+          else if(num > second && num != first){
+              second = num;
+              
+          }
+          
+      }
+      return second == Integer.MIN_VALUE?-1 : second; 
+        
+    }
+}
